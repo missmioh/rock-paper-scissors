a5f8dde23ef05a343804b867d222b3ec0abcb872
missmioh <186388075+missmioh@users.noreply.github.com>
2024-12-05 17:02:35 +0100

    Fix absence of round after wrong input and polish
    Even after a new input, round would not show up in the final result.
    Polish some of the return statements and readability of the code.

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let computerScore = 0;
 function playGame() {
 
     for (let i = 0; i < 5; i++) {
-          const humanSelection = getHumanChoice();
-          const computerSelection = getComputerChoice();
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
         
         playRound(humanSelection, computerSelection);
     }
@@ -13,7 +13,7 @@ function playGame() {
     function playRound(humanChoice, computerChoice) {
         
         if (humanChoice === computerChoice) {
-            console.log("It's a tie! Try again");
+            console.log(`${humanChoice} twice, it's a tie! Try again`);
         } else if ( (humanChoice === "Rock") && (computerChoice=== "Scissors") ) {
             humanScore++;
             console.log("You win! Rock beats Scissors!");
@@ -32,13 +32,15 @@ function playGame() {
         } else if ( (humanChoice === "Scissors") && (computerChoice === "Rock") ) {
             computerScore++;
             console.log("You lose! Rock beats Scissors!");
-        } 
+        } else {
+            playRound(getHumanChoice(), getComputerChoice());
+        }
     }
 
     if (humanScore > computerScore) {
-        console.log("Congratulations, you win!");
+        console.log(`Congratulations, you win with ${humanScore} points!`);
     } else if (humanScore < computerScore) {
-        console.log("You lose, game over!");
+        console.log(`You lose ${computerScore}/${humanScore}, game over!`);
     } else {
         console.log("It's a tie!");
     }
@@ -58,6 +60,7 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
+
     let userInput = prompt("Rock, Paper or Scissors?")
 
         if (userInput.toLowerCase() === "rock") {
@@ -67,6 +70,6 @@ function getHumanChoice() {
         } else if (userInput.toLowerCase() === "scissors") {
             return "Scissors";
         } else {
-            alert("Please type in 'Rock', 'Paper' or 'Scissors!'")
+            alert("Please type only 'Rock', 'Paper' or 'Scissors!'");
         }
 }
